98f8373b82f5a4e50e52cdc5961bf133d2aa7ea5
Eduards Sizovs <eduards@devternity.com>
2024-05-24 20:57:55 +0800

    remove noise from playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -3,16 +3,6 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
-
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-
   reporter: 'html',
   use: {
     baseURL: 'http://127.0.0.1:3001',
