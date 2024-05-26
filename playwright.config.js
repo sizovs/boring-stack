5bc594c9b15b57bfb314ee78d82ada4e5844a061
Eduards Sizovs <eduards@devternity.com>
2024-05-26 19:50:56 +0800

    let playwright have 1 worker per cpu

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   timeout: 3 * 60 * 1000,
   maxFailures: 1,
-  workers: 2,
   testDir: './tests',
   reporter: 'list'
 });
