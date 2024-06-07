d153b05970dc161dda79ed854f9ee7071804aca3
Eduards Sizovs <eduards@devternity.com>
2024-06-07 15:31:54 +0800

    rove type assist

--- application/health/health.js
+++ application/health/health.js
@@ -1,6 +1,3 @@
-/**
- * @param {{ db: import('better-sqlite3').Database }} params
- */
 export const initHealth = ({ router, db }) => {
   router.get('/health', (request, response) => {
     const { health } = db.prepare("select 'OK' as health").get()
