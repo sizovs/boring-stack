d7eccdb83236495af2685e620f427efd93b76d40
Eduards Sizovs <eduards@devternity.com>
2024-06-07 15:29:37 +0800

    add type assist

--- application/health/health.js
+++ application/health/health.js
@@ -1,3 +1,6 @@
+/**
+ * @param {{ db: import('better-sqlite3').Database }} params
+ */
 export const initHealth = ({ router, db }) => {
   router.get('/health', (request, response) => {
     const { health } = db.prepare("select 'OK' as health").get()
