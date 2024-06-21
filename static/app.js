83a648736d333e3e5098d0b545de15ffced7cc57
Eduards Sizovs <eduards@devternity.com>
2024-06-21 14:40:41 +0800

    delete error after 3 secs

--- static/app.js
+++ static/app.js
@@ -3,7 +3,10 @@ const handleServerError = () => {
   error.className = 'flash';
   error.textContent = 'Action failed. Something went wrong while talking to the server.';
   document.body.appendChild(error);
+  setTimeout(() => {
+    document.body.removeChild(error);
+  }, 3000);
 }
 
-document.addEventListener('htmx:sendError', handleServerError);
+document.addEventListener('htmx:afterSwap', handleServerError);
 document.addEventListener('htmx:responseError', handleServerError);
