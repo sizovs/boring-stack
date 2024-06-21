8e49afa2cc080a4df44703463bbfa11bdee3cd4f
Eduards Sizovs <eduards@devternity.com>
2024-06-21 13:20:16 +0800

    simplify flash error

--- static/app.js
+++ static/app.js
@@ -1,9 +1,9 @@
 const handleServerError = () => {
-  const errorHtml = `<div class="flash">Action failed. Something went wrong while talking to the server.</div>`
-  const errorTemplate = document.createElement('template');
-  errorTemplate.innerHTML = errorHtml;
-  const error = errorTemplate.content.firstChild;
+  const error = document.createElement('div');
+  error.className = 'flash';
+  error.textContent = 'Action failed. Something went wrong while talking to the server.';
   document.body.appendChild(error);
 }
+
 document.addEventListener('htmx:sendError', handleServerError);
 document.addEventListener('htmx:responseError', handleServerError);
