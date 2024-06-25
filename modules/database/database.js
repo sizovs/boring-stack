630c7312f455330ad8f7f3c63244a69625092bea
Eduards Sizovs <eduards@devternity.com>
2024-06-25 15:06:17 +0800

    set better SQLite defaults

--- modules/database/database.js
+++ modules/database/database.js
@@ -12,6 +12,16 @@ const createDatabase = (location) => {
   // Litestream takes over checkpointing and recommends running the app with checkpointing disabled:
   // https://litestream.io/tips/#disable-autocheckpoints-for-high-write-load-servers
   db.pragma('wal_autocheckpoint = 0')
+
+  // Enable memory mapped files for speed and smaller memory footprint in multi-connection environments, such as pm2.
+  // https://oldmoe.blog/2024/02/03/turn-on-mmap-support-for-your-sqlite-connections/#benchmark-results
+  // We set 128mb as a reasonable default, but for larger databases, if memory allows, it can go higher.
+  // If it goes too high, the value will be capped at the higher bound enforced by the SQLite at the compile-time.
+  db.pragma(`mmap_size = ${128 * 1024 * 1024}`)
+
+  // Increase cache size to 64mb, the default is 2mb (or slightly higher depending on the SQLite version)
+  db.pragma(`cache_size = ${64 * 1024 * -1}`)
+
   return db
 }
 
