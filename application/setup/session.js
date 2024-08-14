068053b37f24042429728f3436ad40da4bcdf6bb
Eduards Sizovs <eduards@devternity.com>
2024-08-14 22:42:54 +0800

    add cookie-parser middleware

--- application/setup/session.js
+++ application/setup/session.js
@@ -1,5 +1,7 @@
 import cookieSession from 'cookie-session'
+import cookieParser from 'cookie-parser'
 export const enableSessions = ({ app, secret }) => {
+  app.use(cookieParser())
   app.use(cookieSession({
     name: 'session',
     secret
