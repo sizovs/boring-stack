b250fab75bb8c9e06fa2fc498220a98fb33d178e
Eduards Sizovs <eduards@sizovs.net>
2024-10-18 18:11:08 +0800

    perf boost: don't unhash non-static url

--- modules/hasher.js
+++ modules/hasher.js
@@ -1,15 +1,17 @@
 import { join, relative, extname } from 'path'
-import { readFileSync, readdirSync } from 'fs'
+import fs from 'fs'
 import { createHash } from 'crypto'
 
 export class Hasher {
+  #prefix
   #hashes = new Map() // script.js -> script.c040ed4.js
-  constructor({ prefix, root }) {
+  constructor({ prefix, root, filesystem = fs }) {
+    this.#prefix = prefix
     const hash = path => {
-      const file = readFileSync(path)
+      const file = filesystem.readFileSync(path)
       return createHash('md5').update(file).digest('hex').substring(0, 8)
     }
-    files(root).forEach(path => {
+    files(root, filesystem).forEach(path => {
       const original = join(prefix, relative(root, path))
       const ext = extname(path)
       const hashed = `${original.replace(ext, '')}.${hash(path)}${ext}`
@@ -20,13 +22,13 @@ export class Hasher {
     return this.#hashes.get(path) ?? path
   }
   unhashed(path) {
-    return path.replace(/\.([a-zA-Z0-9]{8})\./, '.');
+    return path.startsWith(this.#prefix) ? path.replace(/\.([a-zA-Z0-9]{8})\./, '.') : path
   }
 }
 
-function files(directory) {
-  return readdirSync(directory, { withFileTypes: true }).map(dirent => {
+function files(directory, filesystem) {
+  return filesystem.readdirSync(directory, { withFileTypes: true }).map(dirent => {
     const fullPath = join(directory, dirent.name)
-    return dirent.isDirectory() ? files(fullPath) : fullPath
+    return dirent.isDirectory() ? files(fullPath, filesystem) : fullPath
   }).flat()
 }
