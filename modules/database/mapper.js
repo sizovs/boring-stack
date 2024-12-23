f596e0ee29f9be12a99a74587272ba340e6023b7
Eduards Sizovs <eduards@sizovs.net>
2024-12-23 15:33:11 +0800

    fix mapper.js

--- modules/database/mapper.js
+++ modules/database/mapper.js
@@ -1,11 +1,9 @@
-  // As a performance optimization:
-  // 🔴 Type's constructor is not called.
-  // 🔴 Type's default initializers are not run.
-  // 🔴 Type's private fields are not accessible.
-  // 🟢 Only Types's methods, getters, and setters will be attached to the final instance (+ row's fields, of course).
-export function as(Type) {
+// As a performance optimization:
+// 🔴 Type's constructor is not called.
+// 🔴 Type's default initializers are not run.
+// 🔴 Type's private fields are not accessible.
+// 🟢 Only Types's methods, getters, and setters will be attached to the final instance (+ row's fields, of course).
+export const as = Type => (row) => {
   const instance = Object.create(Type.prototype)
-  return row => {
-    return Object.assign(instance, row)
-  }
+  return Object.assign(instance, row)
 }
--- modules/database/mapper.test.js
+++ modules/database/mapper.test.js
@@ -14,6 +14,7 @@ before(() => {
       creationTs int default (unixepoch())
     ) strict`)
   db.prepare(`insert into users (firstname, lastname) values (?, ?)`).run('Jeff', 'Bezos')
+  db.prepare(`insert into users (firstname, lastname) values (?, ?)`).run('Elon', 'Musk')
 })
 
 class User  {
@@ -29,20 +30,21 @@ describe('mapper', () => {
   const sql = `select *, DATE(creationTs, 'unixepoch') AS creationDate from users`
 
   it('maps rows like a boss 🎉', () => {
-    db.prepare(sql).all().map(as(User)).forEach(assertMapping)
+    assertMappings(db.prepare(sql).all().map(as(User)))
   })
 
-  it('recommends using all() even if one record is retrieved so that map.as(N) can be used', () => {
-    const [user] = db.prepare(sql + ' limit 1').all().map(as(User))
-    assertMapping(user)
-  })
+  const assertMappings = users => {
+    const [jeff, musk] = users
+    assertMapping(jeff, "Jeff Bezos")
+    assertMapping(musk, "Elon Musk")
+  }
 
-  const assertMapping = user => {
+  const assertMapping = (user, whois) => {
     // adds getters
-    assert.equal(user.fullname, 'Jeff Bezos')
+    assert.equal(user.fullname, whois)
 
     // adds methods
-    assert.equal(user.greeting(), 'Hello, Jeff Bezos')
+    assert.equal(user.greeting(), `Hello, ${whois}`)
 
     // adds dynamic fields
     assert.equal(user.creationDate, new Date().toISOString().substring(0, 10))
