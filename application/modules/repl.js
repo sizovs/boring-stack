53e5e00c7efc5ff03d602a1dde98bdee20197200
Eduards Sizovs <eduards@sizovs.net>
2025-04-22 15:16:51 +0800

    provide sql literal to repl

--- application/modules/repl.js
+++ application/modules/repl.js
@@ -2,6 +2,7 @@ import { start } from 'node:repl'
 import { connect } from "#application/modules/database/database"
 
 const repl = start()
-const { db } = await connect(process.env.DB_LOCATION)
+const { db, sql } = await connect(process.env.DB_LOCATION)
 
 repl.context.db = db
+repl.context.sql = sql
