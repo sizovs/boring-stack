e015655beb74cf5536adbfd2f2f74b1889a93979
Eduards Sizovs <eduards@sizovs.net>
2025-05-06 16:22:06 +0800

    demo responsive css

--- application/views/Layout.js
+++ application/views/Layout.js
@@ -9,6 +9,7 @@ export const Layout = Main => params => html`
       <meta name="description" content="A boring todo app" />
       <!-- Scripts -->
       <script src="/static/js/htmx@2.0.4.client.js"></script>
+      <script src="/static/js/css-scope-inline.js"></script>
       <script>
         // The default value includes 'class' as well.
         // We exclude it, otherwise classes added by MutationObserver are being removed during settling
@@ -16,21 +17,6 @@ export const Layout = Main => params => html`
       </script>
 
       <script type="module" src="/static/js/app.client.js" defer></script>
-      <script>
-        // 🌘 CSS Scope Inline (https://github.com/gnat/css-scope-inline)
-        window.cssScopeCount ??= 1 // Let extra copies share the scope count.
-        window.cssScope ??= new MutationObserver(mutations => { // Allow 1 observer.
-          document?.body?.querySelectorAll('style:not([ready])').forEach(node => { // Faster than walking MutationObserver results when receiving subtree (DOM swap, htmx, ajax, jquery).
-            var scope = 'me__'+(window.cssScopeCount++) // Ready. Make unique scope, example: .me__1234
-            node.parentNode.classList.add(scope)
-            node.textContent = node.textContent
-            .replace(/(?:^|\.|(\s|[^a-zA-Z0-9\-\_]))(me)(?![a-zA-Z])/g, '$1.'+scope)
-            .replace(/((@keyframes|animation:|animation-name:)[^{};]*)\.me__/g, '$1me__') // Optional. Removes need to escape names, ex: "\.me"
-
-            node.setAttribute('ready', '')
-          })
-        }).observe(document.documentElement, { childList: true, subtree: true })
-      </script>
 
       <!-- Styles -->
       <link rel="stylesheet" href="/static/css/reset.css" />
@@ -53,6 +39,11 @@ export const Layout = Main => params => html`
             padding: var(--size-8);
             margin: 0 auto;
             max-width: var(--width-lg);
+          }
+          @media sm {
+            me {
+              font-size: var(--scale-2);
+            }
           }
         </style>
         <a href="/" hx-boost="true">boring.todos</a>
