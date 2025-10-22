a8b379da6666d2f97ef680b0856a976ca26014a2
Eduards Sizovs <eduards@sizovs.net>
2025-10-23 00:06:10 +0800

    goodbye unused files

--- application/modules/database/migrate.js
+++
@@ -1,6 +0,0 @@
-import { connect } from "./database.js"
-import { Migrator } from "./migrator.js"
-
-const { db } = await connect(process.env.DB_LOCATION)
-const migrator = new Migrator(db)
-migrator.migrate()
